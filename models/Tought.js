9a2c94d8f82ae259c18a2eee643c6e56da60d75d
kwaters3 <knickler3@gmail.com>
2023-11-26 14:20:47 -0500

    defined though models folder

---
+++ models/Tought.js
@@ -0,0 +1,43 @@
+const {schema, model, Types} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+          type: String,
+          required: "Thought is Required",
+          minlength: 1,
+          maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (currentDate) => dateFormat(currentDate)
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        reactions: [reactionSchema]
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
